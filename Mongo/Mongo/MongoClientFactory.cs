2af1a6d4b0e8247ab4aea7fcc85f386bd8e4f154
Mohamad Reza Safari <Mohamad Reza Safari@DESKTOP-J76TGMQ>
2021-09-12 08:24:01 +0430

    mongo client factory

---
+++ Mongo/Mongo/MongoClientFactory.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Driver;
+
+namespace Mongo
+{
+    public class MongoClientFactory
+    {
+        public MongoClient GetDefaultConstructor()
+        {
+            return new MongoClient();
+        }
+
+        public MongoClient GetConnectionString()
+        {
+            return new MongoClient("mongodb://localhost:27017");
+        }
+
+        public MongoClient GetUrl(bool useUrlCreator = true)
+        {
+            return useUrlCreator
+                ? new MongoClient(MongoUrl.Create("mongodb:localhost:27017"))
+                : new MongoClient(new MongoUrl("mongodb:localhost:27017"));
+        }
+
+        public MongoClient GetSettingFromUrl()
+        {
+            var setting = MongoClientSettings.FromUrl(MongoUrl.Create("mongodb:localhost:27017"));
+            return new MongoClient(setting);
+        }
+
+        public MongoClient GetSetting()
+        {
+            var setting = new MongoClientSettings()
+            {
+                Server = new MongoServerAddress("mongodb:localhost:27017"),
+                //UseSsl = false
+            };
+
+            return new MongoClient(setting);
+        }
+    }
+}
