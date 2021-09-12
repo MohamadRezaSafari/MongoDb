0812eebe1a88bc0756946c65c8bee26a060b1e4c
Mohamad Reza Safari <Mohamad Reza Safari@DESKTOP-J76TGMQ>
2021-09-12 11:34:56 +0430

    mongo db action

---
+++ Mongo/Mongo/MongoClientAction.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+
+namespace Mongo
+{
+    public class MongoClientAction
+    {
+        private readonly MongoClient mongoClient;
+
+        public MongoClientAction(MongoClient mongoClient)
+        {
+            this.mongoClient = mongoClient;
+        }
+
+        public IMongoDatabase GetDatabase(string dbName)
+        {
+            return mongoClient.GetDatabase(dbName);
+        }
+
+        public string DatabaseList()
+        {
+            var result = mongoClient.ListDatabaseNames().ToList();
+
+            return string.Join(',', result);
+        }
+
+        public bool DropDatabase(string dbName)
+        {
+            mongoClient.DropDatabase(dbName);
+
+            return true;
+        }
+    }
+}
---
+++ Mongo/Mongo/MongoDatabaseAction.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Mongo
+{
+    public class MongoDatabaseAction
+    {
+        private readonly IMongoDatabase mongoDatabase;
+
+        public MongoDatabaseAction(IMongoDatabase mongoDatabase)
+        {
+            this.mongoDatabase = mongoDatabase;
+        }
+
+        public void CreateCollection(string collectionName)
+        {
+            mongoDatabase.CreateCollection(collectionName);
+        }
+
+        public string DatabaseList()
+        {
+            var result = mongoDatabase.ListCollectionNames().ToList();
+
+            return string.Join(',', result);
+        }
+
+        public IMongoCollection<BsonDocument> GetCollection(string collectionName)
+        {
+            return mongoDatabase.GetCollection<BsonDocument>(collectionName);
+        }
+    }
+}
